b411e3230dd8f037faaa4ac82374c9a15d667891
Mariana Ramírez Acosta <maria.ramirezacosta9@gmail.com>
2022-08-01 16:58:33 -0500

    variables de carrito de compras

--- assets/js/variables.js
+++ assets/js/variables.js
@@ -5,6 +5,15 @@
 //No usar palabras reservadas para declarar una variable
 // if, return, true, false, for, while, let, var, etc...
 
+//var while;
+//var null ;
+//var if;
+//var true;
+//var false;
+//var var;
+//var const;
+//var let;
+
 // Seguir un standard personal
 
 //Pueden ocntener numeros, letras, guion bajo, simbolo de dolar
@@ -12,3 +21,39 @@ var a;
 var a1;
 var a_nombre;
 var a$Direccion;
+
+//Declaracion de variables
+// Numero
+
+var edad = 24;
+var peso = 78;
+var miopia = 3;
+var astigmatismo = 3;
+
+//Type Boolean
+var ciego = true;
+var hambre = false;
+var bateria = false:
+var ia = true;
+
+//Type String
+var nombre = "Mariana Ramirez";
+var ciudad = "Guadalajara";
+var nacionalidad = "mexicana";
+
+//Type null
+var ganasDeVivir = null;
+var briAmonestaciones = null;
+
+//Variables declaradas con un mismo valor
+var x, y, z = 4;
+
+//Variables undefined
+var xx, yy, zz;
+
+var uno = dos = tres = 4;
+
+
+//Tarea, escribir en una sola linea variables de carrito de compras
+
+var queso, cereal, jamon, pan, leche;
