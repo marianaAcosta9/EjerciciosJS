95d6c1c30ef8132a12394bc1b1f21bec9437960d
Mariana Ramírez Acosta <maria.ramirezacosta9@gmail.com>
2022-08-01 16:40:46 -0500

    Se agrego archivo para ver tipos de variables

---
+++ assets/js/variables.js
@@ -0,0 +1,14 @@
+//Mejores practicas
+//camelCase: Utilizar mayusulas en la mitad de nuestras variables, ejemplo: marianaVariableUno
+
+//Palabras reservadas
+//No usar palabras reservadas para declarar una variable
+// if, return, true, false, for, while, let, var, etc...
+
+// Seguir un standard personal
+
+//Pueden ocntener numeros, letras, guion bajo, simbolo de dolar
+var a;
+var a1;
+var a_nombre;
+var a$Direccion;
