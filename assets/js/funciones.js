21dc5692f73358dae0da82ae59367754846cfa4e
Mariana Ramírez Acosta <maria.ramirezacosta9@gmail.com>
2022-08-02 17:09:39 -0500

    input y Output

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -39,10 +39,55 @@ globalScope();
 //LET
 //Scope global, local
 
+//console.log(a);
+//let a = "Hoisting let";
+
 //VAR
 //Sope global, local
 //Hoisting: no se puede utilizar o invocar antes de declarar
 
+//console.log(x);
+//var x = "hola";
+
 //CONST
 //Scope global, local
 //No se puede modificar
+
+//console.log(c);
+//const c = "Hoisting const";
+
+//ejemplo de Input
+
+//Se declara primero la variable global
+const grito = "AAaaaaaAAAaaa";
+
+function fiuFiu(x) {
+    console.log(x);
+}
+
+fiuFiu(grito);
+fiuFiu("mariana ramírez acosta");
+fiuFiu(2);
+
+function dosParametros(x,y) {
+    console.log(x);
+    console.log(y);
+}
+
+dosParametros("Hola","Qué tal?");
+
+
+//Ejemplo de Output
+
+function out() {
+    let x = "Hi"
+    return x;
+}
+
+console.log(out());
+
+//Opción 2
+
+const receive = out();
+
+console.log(receive);
