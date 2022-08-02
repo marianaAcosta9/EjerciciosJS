a491e66a049055013ebf2b22ed66ac13112e25f8
Mariana Ramírez Acosta <maria.ramirezacosta9@gmail.com>
2022-08-02 15:36:12 -0500

    se creó una función que imprime Hola Mundo

---
+++ assets/js/funciones.js
@@ -0,0 +1,13 @@
+// 1. Utilizar keyword function
+// 2. Nombrar nuestra función
+// 3. Utilizar () y abrir y cerrar la función
+
+// Mejores Prácticas: Simplificar funciones lo más posible.
+
+function myFunction() {
+    //Aquí va el cuerpo de la función
+    console.log("Hola Mundo")
+}
+
+//Después de que se declara, se invoca la función:
+myFunction();
