064511f417b7aeae60ae66d929785585cf3dfb32
Mariana Ramírez Acosta <maria.ramirezacosta9@gmail.com>
2022-08-02 16:28:53 -0500

    funciones localScope y globalScope

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -11,3 +11,38 @@ function myFunction() {
 
 //Después de que se declara, se invoca la función:
 myFunction();
+
+
+//Ejemplo2, segunda función
+function myName() {
+    console.log("Marianita")
+}
+
+myName();
+
+function scopeFunction() {
+    let local = "Esto está dentro de la función";
+    console.log(local);
+}
+
+scopeFunction();
+
+let nombre = "mariana rmz";
+function globalScope() {
+    console.log(nombre);
+}
+
+globalScope();
+
+//Scope de variables:
+
+//LET
+//Scope global, local
+
+//VAR
+//Sope global, local
+//Hoisting: no se puede utilizar o invocar antes de declarar
+
+//CONST
+//Scope global, local
+//No se puede modificar
